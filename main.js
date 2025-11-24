ea0ae14eab8ff555a9abf70be0b314044853566c
LorranyDantas09 <lorranys1000@gmail.com>
2025-11-24 16:12:04 -0300

    mensagem do commit

--- main.js
+++ main.js
@@ -12,7 +12,14 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  dialect: "postgres",
+  port: Number(process.env.DB_PORT),
+  dialectOptions: {
+    ssl:{
+      reaquire: true, rejecUnauthorized: false}
+
+  },
+  logging: false
 });
 
 // Listar agendamentos
