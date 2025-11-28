ef23b2aea97234379711a3a7e6d0c1f139da9a17
LorranyDantas09 <lorranys1000@gmail.com>
2025-11-28 16:11:04 -0300

    1

--- public/js/ass.js
+++ public/js/ass.js
@@ -5,3 +5,24 @@ function openMenu() {
       function closeMenu() {
         document.getElementById("menuLateral").style.width = "0";
       }
+
+      
+      const botaoAssinar = document.querySelector('.btn-assinar');
+      const loading = document.getElementById('loading');
+  
+      botaoAssinar.addEventListener('click', () => {
+        const confirmar = confirm("Deseja realmente se tornar um assinante do Clube Detroit?");
+  
+        if (confirmar) {
+          
+          loading.classList.add('ativo');
+  
+          
+          setTimeout(() => {
+            window.location.href = "assp.html";
+          }, 0);
+        } else {
+          alert("A assinatura foi cancelada.");
+        }
+      });
+   
